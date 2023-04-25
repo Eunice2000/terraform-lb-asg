8640f742cb5e5b12485eb0467928341303580680
Eunice2000 <adediraneunice20@gmail.com>
2023-04-25 15:13:27 +0100

    work

--- infra/provider.tf
+++ infra/provider.tf
@@ -5,11 +5,7 @@ terraform {
       version = "~>4.0"
     }
   }
-  backend "s3" {
-    bucket = "terraform1-assignment"
-    key    = "aws/terraform1/terraform.tfstate"
-    region = "us-east-1"
-  }
+  
 }
 
 provider "aws" {
